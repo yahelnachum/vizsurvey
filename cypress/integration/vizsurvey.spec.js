a4c196c11714309fd7012091faff7d82899b3ac5
Yahel Nachum <yahelnachum@yahoo.com>
2022-03-21 17:29:45 -0400

    fixed cypress tests

--- cypress/integration/vizsurvey.spec.js
+++ cypress/integration/vizsurvey.spec.js
@@ -4,23 +4,22 @@ describe("vizsurvey", () => {
   it("survey", () => {
     cy.viewport(1200, 700);
     cy.visit("http://localhost:3000");
-    //<p><a href="http://localhost:3000/vizsurvey?treatment_id=2">localhost</a></p>
     cy.get("#getQuestionSet").click();
     cy.wait(1000);
     cy.get(".nav-link").contains("Survey").click();
-    cy.get("label").contains("$200 today").click();
-    cy.get("button").contains("Submit").click();
+    cy.get("#id0").click();
+    cy.get("#id3").click();
+    // cy.get("button").contains("Submit").click();
     cy.get("p").contains("Thank you").should("exist");
 
     cy.viewport(800, 600);
     cy.visit("http://localhost:3000");
-    //<p><a href="http://localhost:3000/vizsurvey?treatment_id=2">localhost</a></p>
     cy.get("#getQuestionSet").click();
     cy.get(".navbar-toggler-icon").click();
     cy.get(".nav-link").contains("Survey").click();
     cy.get(".navbar-toggler-icon").click();
-    cy.get("label").contains("$200 today").click();
-    cy.get("button").contains("Submit").click();
+    cy.get("#id0").click();
+    cy.get("#id3").click();
     cy.get("p").contains("Thank you").should("exist");
   });
 });
