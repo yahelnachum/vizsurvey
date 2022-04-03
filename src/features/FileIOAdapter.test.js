af03875c86a0ecfa1d7790015d7be91150805cab
Peter Cordone <pcordone@gmail.com>
2022-04-03 10:06:39 -0400

    Added a second answer instance to unit test.
    Added a 2nd answer instance to unit test to make sure it works with an array of more than one element in answer.

--- src/features/FileIOAdapter.test.js
+++ src/features/FileIOAdapter.test.js
@@ -36,11 +36,11 @@ describe("FileIOAdapter tests", () => {
   });
 
   test("Validate treatment CSV fields are loaded correctly.", async () => {
-    const answer = new Answer({
+    const answer1 = new Answer({
       treatmentId: 1,
       position: 2,
-      viewType: ViewType.barchart,
-      interaction: InteractionType.drag,
+      viewType: ViewType.word,
+      interaction: InteractionType.none,
       amountEarlier: 3,
       timeEarlier: 4,
       dateEarlier: DateTime.utc(2001, 1, 1, 1, 1, 1, 1),
@@ -58,12 +58,35 @@ describe("FileIOAdapter tests", () => {
       lowdown: 12,
       participantCode: "participant code",
     });
-    const answers = [answer];
+    const answer2 = new Answer({
+      treatmentId: 13,
+      position: 14,
+      viewType: ViewType.barchart,
+      interaction: InteractionType.drag,
+      amountEarlier: 15,
+      timeEarlier: 16,
+      dateEarlier: DateTime.utc(2001, 1, 2, 1, 1, 1, 1),
+      amountLater: 17,
+      timeLater: 18,
+      dateLater: DateTime.utc(2001, 1, 2, 2, 1, 1, 1),
+      maxAmount: 19,
+      maxTime: 20,
+      verticalPixels: 21,
+      horizontalPixels: 22,
+      choice: ChoiceType.later,
+      shownTimestamp: DateTime.utc(2001, 1, 2, 3, 1, 1, 1),
+      choiceTimestamp: DateTime.utc(2001, 1, 2, 4, 1, 1, 1),
+      highup: 23,
+      lowdown: 24,
+      participantCode: "participant code 2",
+    });
+    const answers = [answer1, answer2];
     const io = new FileIOAdapter();
     const result = io.convertToCSV(answers);
     console.log(result);
     expect(result)
       .toBe(`treatment_id,position,view_type,interaction,amount_earlier,time_earlier,date_earlier,amount_later,time_later,date_later,max_amount,max_time,vertical_pixels,horizontal_pixels,choice,shown_timestamp,choice_timestamp,highup,lowdown,participant_code
-1, 2, ViewType.barchart, InteractionType.drag, 3, 4, 2001-01-01T01:01:01.001Z, 5, 6, 2001-01-01T02:01:01.001Z, 7, 8, 9, 10, earlier, 2001-01-01T03:01:01.001Z, 2001-01-01T04:01:01.001Z, 11, 12, participant code`);
+1, 2, ViewType.word, InteractionType.none, 3, 4, 2001-01-01T01:01:01.001Z, 5, 6, 2001-01-01T02:01:01.001Z, 7, 8, 9, 10, earlier, 2001-01-01T03:01:01.001Z, 2001-01-01T04:01:01.001Z, 11, 12, participant code
+13, 14, ViewType.barchart, InteractionType.drag, 15, 16, 2001-01-02T01:01:01.001Z, 17, 18, 2001-01-02T02:01:01.001Z, 19, 20, 21, 22, later, 2001-01-02T03:01:01.001Z, 2001-01-02T04:01:01.001Z, 23, 24, participant code 2`);
   });
 });
