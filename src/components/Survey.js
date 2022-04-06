86b0cb004dea64a65b3bbe0cb964be667e58c6fb
Peter Cordone <pcordone@gmail.com>
2022-04-05 20:59:12 -0400

    Removed flex and got the graphs to center vertically.
    Also removed unused css in style sheet.

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,4 +1,3 @@
-import { Container, Row, Col } from "react-bootstrap";
 import MELForm from "./MELForm";
 import BarChart from "./BarChart";
 import Calendar from "./Calendar";
@@ -9,30 +8,35 @@ import { selectCurrentQuestion } from "../features/questionSlice";
 export function Survey() {
   const question = useSelector(selectCurrentQuestion);
 
+  // Got from https://stackoverflow.com/questions/31217268/center-div-on-the-middle-of-screen
+  const divCenterContentStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  };
+
   return (
-    <Container fluid>
-      <Row>
-        <Col>
-          {question.viewType === ViewType.barchart ? (
-            <BarChart
-              top_margin="20"
-              right_margin="30"
-              bottom_margin="30"
-              left_margin="80"
-            />
-          ) : question.viewType === ViewType.word ? (
-            <MELForm />
-          ) : (
-            <Calendar
-              top_margin="20"
-              right_margin="20"
-              bottom_margin="30"
-              left_margin="30"
-            />
-          )}
-        </Col>
-      </Row>
-    </Container>
+    <div style={divCenterContentStyle}>
+      {question.viewType === ViewType.barchart ? (
+        <BarChart
+          top_margin="20"
+          right_margin="30"
+          bottom_margin="30"
+          left_margin="80"
+        />
+      ) : question.viewType === ViewType.word ? (
+        <MELForm />
+      ) : (
+        <Calendar
+          top_margin="20"
+          right_margin="20"
+          bottom_margin="30"
+          left_margin="30"
+        />
+      )}
+    </div>
   );
 }
 
